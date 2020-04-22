1192149cb41fc478b2fb2cd687dd7511a11c6787
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-22 23:28:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
  const Engine = Matter.Engine;
  const World = Matter.World;
- const Bodies=Matter.Bodies;
- var engine,world;
+ const Bodies = Matter.Bodies;
+ var engine, world;
  var box1;
  function setup() {
   var canvas =createCanvas(400,400);
-  engine=Engine.create();
-  world=engine.world;
-  box1=new Box();
+  engine = Engine.create();
+  world = engine.world;
+  box1 = new Box();
   
  }
 
