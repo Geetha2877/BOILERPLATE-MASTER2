903727a7b87acb29bb2000a7be2dd05532fbb73c
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-23 10:42:38 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,7 +1,7 @@
 class Box {
     constructor() {
         var options = {
-            restitution: 1.0
+            restitution: 1;
     }
         this.body = Bodies.rectangle(200,100,50,50,options);    
         World.add(world, this.body);
