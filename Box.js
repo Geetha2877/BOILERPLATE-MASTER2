d5a569f189798ed47888896fb8890174854406f3
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-04-23 10:02:27 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -9,7 +9,7 @@ class Box {
     display() {
         var pos = this.body.position;
         rectMode(CENTER);
-        fill(255);
+      //  fill(255);
         rect(pos.x, pos.y, this.width, this.height);
 
     }
